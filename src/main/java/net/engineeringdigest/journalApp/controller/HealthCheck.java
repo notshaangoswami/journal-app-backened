ff41d8ad8b6cf0d595b4482e8bc58046eda0c822
Shaan Goswami <shaan.goswami@sap.com>
2025-03-07 15:07:29 +0530

    new page

--- src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
+    @GetMapping
+    public String topPage(){
+        return "health check";
+    }
     @GetMapping("/hello")
     public String healthCheck(){
         return "health check";
